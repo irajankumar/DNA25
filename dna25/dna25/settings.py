18fe10d08b6aebb6a370b6a9764bfafc5ba16b05
rkking <rkking@live.com>
2019-11-17 00:12:49 +0530

    added req text

--- dna25/dna25/settings.py
+++ dna25/dna25/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e#2k=ds%a_5qvv64^n$wm6+yxukdo4@&kiceamiblb@s2m&ktk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
